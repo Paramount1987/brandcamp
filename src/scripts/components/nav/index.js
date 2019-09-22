ebd5d8f28a767e5ec1b00e869f8a64322841cea2
Artem Petrov <Paramount7891@gmail.com>
2019-09-22 13:17:46 +0300

    != fix slider & nav

--- src/scripts/components/nav/index.js
+++ src/scripts/components/nav/index.js
@@ -1,9 +1,14 @@
 const Nav = {
   init() {
+    this.$btnCatalog = $('.js-header-ct');
+    this.$headerCatalog = $('.js-header-catalog');
+
     this.$btnNav = $('.js-btn-nav');
     this.$nav = $('.js-nav-mbl');
     this.btnClickHandler();
-    this.navLinkHandle();
+    this.navLinkHandler();
+    this.btnCatalogHandler();
+    this.clickOutsideHandler();
   },
 
   btnClickHandler() {
@@ -15,13 +20,41 @@ const Nav = {
     });
   },
 
-  navLinkHandle() {
+  navLinkHandler() {
     $('.js-nav-link, .js-nav-lng').click(function handler(e) {
       e.preventDefault();
       const $el = $(this);
       const $target = $($el.attr('href'));
 
       $target.slideToggle(200);
+    });
+  },
+
+  btnCatalogHandler() {
+    const self = this;
+
+    this.$btnCatalog.click((e) => {
+      e.preventDefault();
+      self.$headerCatalog.slideToggle();
+    });
+  },
+
+  clickOutsideHandler() {
+    const self = this;
+
+    $(document).on('click', (e) => {
+      const $target = $(e.target);
+
+      if (!self.$btnCatalog.is($target) && !$target.closest(self.$btnCatalog).length) {
+        self.$headerCatalog.slideUp();
+      }
+
+      if (!$target.closest('.js-nav-lng').length) {
+        $('#nav-lng').slideUp();
+      }
+      if (!$target.closest('.js-nav-link').length) {
+        $('#nav-about').slideUp();
+      }
     });
   },
 };
--- src/scripts/components/sliders/pdct-sliders.js
+++ src/scripts/components/sliders/pdct-sliders.js
@@ -1,13 +1,46 @@
 /* global Swiper */
 
-const swiper = new Swiper('.js-pdct-gallery', {
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
+$('.js-gallery-wrapper').each(function init() {
+  const $wrapper = $(this);
+  const $mainGallery = $wrapper.find('.js-pdct-gallery');
+  const $verticalGallery = $wrapper.find('.js-pdct-vertical-gallery');
 
-const verticalSwiper = new Swiper('.js-pdct-vertical-gallery', {
-  direction: 'vertical',
-  slidesPerView: 4,
-  spaceBetween: 10,
+  const $verticalSlides = $verticalGallery.find('.swiper-slide');
+  const $arrowVerticalPrev = $wrapper.find('.js-vertical-prev');
+  const $arrowVerticalNext = $wrapper.find('.js-vertical-next');
+
+  const mainSwiper = new Swiper($mainGallery, {
+    simulateTouch: false,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+  const verticalSwiper = new Swiper($verticalGallery, {
+    direction: 'vertical',
+    slidesPerView: 4,
+    spaceBetween: 10,
+  });
+
+  // curr click vertical slide
+  $verticalSlides.click(function initClickSlide() {
+    const $currSlide = $(this);
+
+    $verticalSlides.removeClass('is-active');
+    $currSlide.addClass('is-active');
+    const currInd = $currSlide.data('rel');
+
+    mainSwiper.slideTo(+currInd);
+  });
+
+  //--------------------------------
+
+  // click prev & next arrow
+  $arrowVerticalPrev.click(() => {
+    verticalSwiper.slidePrev();
+  });
+
+  $arrowVerticalNext.click(() => {
+    verticalSwiper.slideNext();
+  });
 });
