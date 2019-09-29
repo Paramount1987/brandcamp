8a080097b6f3e252aa28544866e9a92921ac53cf
Artem Petrov <Paramount7891@gmail.com>
2019-09-29 11:42:01 +0300

    + lk returns page

--- src/scripts/components/sliders/multy-slider.js
+++ src/scripts/components/sliders/multy-slider.js
@@ -1,23 +1,29 @@
 /* global Swiper */
 
-const swiper = new Swiper('.js-multy-slider', {
-  loop: true,
-  slidesPerView: 4,
-  spaceBetween: 30,
-  simulateTouch: false,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is <= 779px
-    779: {
-      slidesPerView: 2,
+$('.js-multy-slider').each(function initSlider() {
+  const el = this;
+  const perViewLg = +$(el).data('perviewlg') || 4;
+  const perViewMd = +$(el).data('perviewmd') || 3;
+
+  const swiper = new Swiper(el, {
+    loop: true,
+    slidesPerView: perViewLg,
+    spaceBetween: 30,
+    simulateTouch: false,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
     },
-    // when window width is <= 1279px
-    1279: {
-      slidesPerView: 3,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is <= 779px
+      779: {
+        slidesPerView: 2,
+      },
+      // when window width is <= 1279px
+      1279: {
+        slidesPerView: perViewMd,
+      },
     },
-  },
+  });
 });
